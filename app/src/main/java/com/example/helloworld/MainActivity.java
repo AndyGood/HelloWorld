22a95957a6b1a53f4ed82a35d4a6a3bc49c29474
laihaipeng <lai_haipeng@163.com>
2016-05-27 14:30:03 +0800

    test commit

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    public void test(){
+        Toast.makeText(MainActivity.this, "this is a test!", Toast.LENGTH_SHORT).show();
     }
 }
